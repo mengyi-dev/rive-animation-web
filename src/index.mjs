c8a4e3633e5a3d3dead178fa1a7474f512c15bc5
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-11-06 11:14:38 +0700

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -15,7 +15,7 @@ const riveCanvas = document.getElementById("rive-canvas")
 
 const riveInstance = new rive.Rive({
   // Load a local riv `clean_the_car.riv` or upload your own!
-  src: "./clean_the_car.riv",
+  src: "https://public.rive.app/community/runtime-files/2732-5606-clean-the-car.riv",
   // Be sure to specify the correct state machine (or animation) name
   stateMachines: "Motion", // Name of the State Machine to play
   canvas: riveCanvas,
